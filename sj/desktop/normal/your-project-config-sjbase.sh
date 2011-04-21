1e26a67d4521f037fb532ca10bc3d9d48e908e61
Jerry <gisanfu@gmail.com>
2011-04-21 16:48:35 +0800

    fix bug

--- sj/desktop/normal/your-project-config-sjbase.sh
+++ sj/desktop/normal/your-project-config-sjbase.sh
@@ -3,4 +3,4 @@
 # 這裡放的是設定檔重設路徑的位置
 # 目的是要跟隨專案資料夾，跟著帶走
 
-fast_change_dir_your_project_config=~/hg/sjbase/misc/fast-change-dir
+fast_change_dir_your_project_config=~/hg/sj_customcase_base/misc/fast-change-dir
